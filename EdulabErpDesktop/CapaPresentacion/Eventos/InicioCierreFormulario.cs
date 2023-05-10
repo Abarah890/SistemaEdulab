2d4e70a40e216f77978f177605db40f36f322151
Abarah890 <alvarito890.h@gmail.com>
2023-05-10 15:01:03 -0600

    Evento InicioCierreFormulario
    El cambio contiene el diseño de la clase de tipo evento para el efecto de apertura y cierre de los formularios con sus respectivos métodos (EfectoInicio y EfectoCierre)

--- EdulabErpDesktop/CapaPresentacion/Eventos/InicioCierreFormulario.cs
+++ EdulabErpDesktop/CapaPresentacion/Eventos/InicioCierreFormulario.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CapaPresentacion.Eventos
 {
     internal class InicioCierreFormulario
     {
+        public static void EfectoInicio(Form Formulario, int Intervalo = 10)
+        {
+            double Opacidad;
+            for (Opacidad = 0; Opacidad <= 1; Opacidad += 0.05)
+            {
+                Formulario.Opacity = Opacidad;
+                Formulario.Refresh();
+                System.Threading.Thread.Sleep(Intervalo);
+            }
+
+            Formulario.Opacity = 1;
+
+        }
+
+        public static void EfectoCierre(Form Formulario, int Intervalo = 10)
+        {
+            double Opacidad;
+            for (Opacidad = 1; Opacidad <= 0; Opacidad -= 0.05)
+            {
+                Formulario.Opacity = Opacidad;
+                Formulario.Refresh();
+                System.Threading.Thread.Sleep(Intervalo);
+            }
+
+            Formulario.Opacity = 0;
+
+        }
 
     }
 }
--- EdulabErpDesktop/CapaPresentacion/Modulos/InicioSesion.cs
+++ EdulabErpDesktop/CapaPresentacion/Modulos/InicioSesion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaPresentacion.Eventos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,12 +20,20 @@ namespace CapaPresentacion
             this.StartPosition = FormStartPosition.CenterScreen;
             this.ControlBox = false;
             PbxLogoEdulab.BorderRadius = 25;
+            BtnCerrarAplicacion.Visible = false;
 
         }
 
         private void BtnCerrarAplicacion_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void InicioSesion_Load(object sender, EventArgs e)
+        {
+            InicioCierreFormulario.EfectoInicio(this);
+            System.Threading.Thread.Sleep(1000);
+            BtnCerrarAplicacion.Visible = true;
         }
     }
 }
